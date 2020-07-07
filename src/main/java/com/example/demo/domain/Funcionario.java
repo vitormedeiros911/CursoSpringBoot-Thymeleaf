7494f91c8e303294485028c891331cd870937f5a
vitormedeiros911 <vitormedeiros911@outlook.com>
2020-07-07 10:46:29 -0300

    Finaliza a adição de validação

--- src/main/java/com/example/demo/domain/Funcionario.java
+++ src/main/java/com/example/demo/domain/Funcionario.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
 
 import org.springframework.format.annotation.NumberFormat;
 import org.springframework.format.annotation.NumberFormat.Style;
@@ -15,75 +17,81 @@ import org.springframework.format.annotation.DateTimeFormat.ISO;
 @Table(name = "FUNCIONARIOS")
 public class Funcionario extends AbstractEntity<Long> {
 
+  @NotBlank
+	@Size(max = 255, min = 3)
 	@Column(nullable = false, unique = true)
 	private String nome;
 
-  @NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
+	@NotNull
+	@NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
 	@Column(nullable = false, columnDefinition = "DECIMAL(7,2) DEFAULT 0.00")
 	private BigDecimal salario;
 
-  @DateTimeFormat(iso = ISO.DATE)
-	@Column(name = "data_entrada", nullable = false, columnDefinition = "DATE")
+	@NotNull
+	@PastOrPresent(message = "{PastOrPresent.funcionario.dataEntrada}")
+	@DateTimeFormat(iso = ISO.DATE)
+	@Column(name= "data_entrada", nullable = false, columnDefinition = "DATE")
 	private LocalDate dataEntrada;
 
-  @DateTimeFormat(iso = ISO.DATE)
+	@DateTimeFormat(iso = ISO.DATE)
 	@Column(name = "data_saida", columnDefinition = "DATE")
 	private LocalDate dataSaida;
 
+	@Valid
 	@OneToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name = "ender"
-			+ "eco_id_fk")
+	@JoinColumn(name = "endereco_id_fk")
 	private Endereco endereco;
 
+	@NotNull(message = "{NotNull.funcionario.cargo}")
 	@ManyToOne
 	@JoinColumn(name = "cargo_id_fk")
 	private Cargo cargo;
 
-	public String getNome() {
-		return nome;
-	}
+  public String getNome() {
+    return nome;
+  }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 
-	public BigDecimal getSalario() {
-		return salario;
-	}
+  public BigDecimal getSalario() {
+    return salario;
+  }
 
-	public void setSalario(BigDecimal salario) {
-		this.salario = salario;
-	}
+  public void setSalario(BigDecimal salario) {
+    this.salario = salario;
+  }
 
-	public LocalDate getDataEntrada() {
-		return dataEntrada;
-	}
+  public LocalDate getDataEntrada() {
+    return dataEntrada;
+  }
 
-	public void setDataEntrada(LocalDate dataEntrada) {
-		this.dataEntrada = dataEntrada;
-	}
+  public void setDataEntrada(LocalDate dataEntrada) {
+    this.dataEntrada = dataEntrada;
+  }
 
-	public LocalDate getDataSaida() {
-		return dataSaida;
-	}
+  public LocalDate getDataSaida() {
+    return dataSaida;
+  }
 
-	public void setDataSaida(LocalDate dataSaida) {
-		this.dataSaida = dataSaida;
-	}
+  public void setDataSaida(LocalDate dataSaida) {
+    this.dataSaida = dataSaida;
+  }
 
-	public Endereco getEndereco() {
-		return endereco;
-	}
+  public Endereco getEndereco() {
+    return endereco;
+  }
 
-	public void setEndereco(Endereco endereco) {
-		this.endereco = endereco;
-	}
+  public void setEndereco(Endereco endereco) {
+    this.endereco = endereco;
+  }
 
-	public Cargo getCargo() {
-		return cargo;
-	}
+  public Cargo getCargo() {
+    return cargo;
+  }
 
-	public void setCargo(Cargo cargo) {
-		this.cargo = cargo;
-	}
+  public void setCargo(Cargo cargo) {
+    this.cargo = cargo;
+  }
 }
--- src/main/java/com/example/demo/web/controller/FuncionarioController.java
+++ src/main/java/com/example/demo/web/controller/FuncionarioController.java
@@ -3,17 +3,23 @@ package com.example.demo.web.controller;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import com.example.demo.domain.Cargo;
 import com.example.demo.domain.Funcionario;
 import com.example.demo.domain.UF;
 import com.example.demo.service.CargoService;
 import com.example.demo.service.FuncionarioService;
+import com.example.demo.web.validator.FuncionarioValidator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,23 +37,33 @@ public class FuncionarioController {
   @Autowired
   private CargoService cargoService;
 
+  @InitBinder
+  public void initBinder(WebDataBinder binder) {
+    binder.addValidators(new FuncionarioValidator());
+  }
+
   @GetMapping("/cadastrar")
   public String cadastrar(Funcionario funcionario) {
-    return "/funcionario/cadastro";
+    return "funcionario/cadastro";
   }
 
   @GetMapping("/listar")
   public String listar(ModelMap model) {
     model.addAttribute("funcionarios", funcionarioService.buscarTodos());
-    return "/funcionario/lista";
+    return "funcionario/lista";
   }
 
   @PostMapping("/salvar")
-  public String salvar(Funcionario funcionario, RedirectAttributes attr) {
-    funcionarioService.salvar(funcionario);
-    attr.addFlashAttribute("success", "Funcionário inserido com sucesso.");
-    return "redirect:/funcionarios/cadastrar";
-  }
+	public String salvar(@Valid Funcionario funcionario, BindingResult result, RedirectAttributes attr) {
+
+		if (result.hasErrors()) {
+			return "/funcionario/cadastro";
+		}
+
+		funcionarioService.salvar(funcionario);
+		attr.addFlashAttribute("success", "Funcionário inserido com sucesso.");
+		return "redirect:/funcionarios/cadastrar";
+	}
 
   @GetMapping("/editar/{id}")
   public String preEditar(@PathVariable("id") Long id, ModelMap model) {
@@ -56,11 +72,16 @@ public class FuncionarioController {
   }
 
   @PostMapping("/editar")
-  public String editar(Funcionario funcionario, RedirectAttributes attr) {
-    funcionarioService.editar(funcionario);
-    attr.addFlashAttribute("success", "Funcionário removido com sucesso.");
-    return "redirect:/funcionarios/listar";
-  }
+	public String editar(@Valid Funcionario funcionario, BindingResult result, RedirectAttributes attr) {
+
+		if (result.hasErrors()) {
+			return "/funcionario/cadastro";
+		}
+
+		funcionarioService.editar(funcionario);
+		attr.addFlashAttribute("success", "Funcionário editado com sucesso.");
+		return "redirect:/funcionarios/cadastrar";
+	}
 
   @GetMapping("/excluir/{id}")
   public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
